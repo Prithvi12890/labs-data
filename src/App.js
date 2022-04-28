6584667269adce5b67310f8ded7644a262031913
Prithvi Sandeep <prithvisandeep.saladi@gmail.com>
2022-04-28 23:18:00 +0530

    Mobile view enaled, MWE added

--- src/App.js
+++ src/App.js
@@ -44,18 +44,19 @@ function App() {
       // {title:"Experiment 9", id: "tab-9", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mc/record/exp9.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mc/outputs/exp9.pdf"},
       // {title:"Experiment 10", id: "tab-10", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mc/record/exp10.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mc/outputs/exp10.pdf"},
   ];
-  // const mweExpData = [
-  //   {title:"Experiment 1", id: "tab-1", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp1.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp1.pdf"},
-  //   {title:"Experiment 2", id: "tab-2", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp2.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp2.pdf"},
-  //   {title:"Experiment 3", id: "tab-3", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp3.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp3.pdf"},
-  //   {title:"Experiment 4", id: "tab-4", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp4.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp4.pdf"},
-  //   {title:"Experiment 5", id: "tab-5", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp5.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp5.pdf"},
-  //   {title:"Experiment 6", id: "tab-6", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp6.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp6.pdf"},
-  //   {title:"Experiment 7", id: "tab-7", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp7.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp7.pdf"},
-  //   {title:"Experiment 8", id: "tab-8", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp8.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp8.pdf"},
-  //   {title:"Experiment 9", id: "tab-9", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp9.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp9.pdf"},
-  //   {title:"Experiment 10", id: "tab-10", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp10.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp10.pdf"},
-  // ];
+  const mweExpData = [
+    {title:"Lab Manual", id: "tab-0", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/lab-manual.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/lab-manual.pdf"},
+    {title:"V.S.W.R. Measurement", id: "tab-1", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/vswr.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/vswr.pdf"},
+    {title:"Wavelength & Freq Measurement", id: "tab-2", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/waveFreq.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/waveFreq.pdf"},
+    {title:"Reflex Klystron", id: "tab-3", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/klystron.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/klystron.pdf"},
+    // {title:"Experiment 4", id: "tab-4", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp4.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp4.pdf"},
+    // {title:"Experiment 5", id: "tab-5", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp5.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp5.pdf"},
+    // {title:"Experiment 6", id: "tab-6", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp6.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp6.pdf"},
+    // {title:"Experiment 7", id: "tab-7", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp7.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp7.pdf"},
+    // {title:"Experiment 8", id: "tab-8", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp8.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp8.pdf"},
+    // {title:"Experiment 9", id: "tab-9", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp9.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp9.pdf"},
+    // {title:"Experiment 10", id: "tab-10", code:"sdf", pdfLink: "http://prithvi12890.github.io/lab-data/pdfs/mwe/record/exp10.pdf", outputs:"http://prithvi12890.github.io/lab-data/pdfs/mwe/outputs/exp10.pdf"},
+  ];
   return (
     <div className="App">
         <BrowserRouter>
@@ -73,7 +74,7 @@ function App() {
                 <Route path="/labs-data/home" element={<LabCards/>} />
                 <Route path="/labs-data/home/dsp" element={<ExpCards expData={dspExpData}/>} />
                 <Route path="/labs-data/home/mc" element={<ExpCards expData={mcExpData}/>} />
-                {/* <Route path="/labs-data/home/mwe" element={<ExpCards expData={mweExpData}/>} /> */}
+                <Route path="/labs-data/home/mwe" element={<ExpCards expData={mweExpData}/>} />
                 <Route path="*" element={<ErrorPage/>} />
             </Routes>
 
